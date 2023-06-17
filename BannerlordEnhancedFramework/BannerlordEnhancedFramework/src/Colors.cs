611dc4b1e9914ece9110b88c686cf1748a0c7bab
ricomaster9000 <ricorpwd@yahoo.co.za>
2023-06-17 14:47:21 +0200

    DebugUtils logging methods now also push to game information console

---
+++ BannerlordEnhancedFramework/BannerlordEnhancedFramework/src/Colors.cs
@@ -0,0 +1,11 @@
+﻿using TaleWorlds.Library;
+
+namespace BannerlordEnhancedFramework;
+
+public class Colors
+{
+    public static Color Black => new Color(0.0f, 0.0f, 0.0f);
+    public static Color White => new Color(1f, 1f, 1f);
+    public static Color Red => new Color(1.0f, 0.0f, 0.0f);
+    public static Color Yellow => new Color(1f, 1f, 0f);
+}
