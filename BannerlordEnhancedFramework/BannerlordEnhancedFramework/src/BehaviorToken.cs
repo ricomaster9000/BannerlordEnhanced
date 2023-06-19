4930fef8a47e50bf03b9d6ac5137d99cf7a3018e
ricomaster9000 <ricorpwd@yahoo.co.za>
2023-06-19 21:10:17 +0200

    added BehaviorToken which stores known base game behaviour re-usable logic and ExtendedTimer for use in background tasks

---
+++ BannerlordEnhancedFramework/BannerlordEnhancedFramework/src/BehaviorToken.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BannerlordEnhancedFramework;
+
+public class BehaviorTokens
+{
+    // A-Z
+
+    // every hero should have this dialogue option, base game dialogue
+    public static BehaviorToken HeroMainOptions = new BehaviorToken("hero_main_options");
+
+    // base game dialogue
+    public static BehaviorToken MerchantWeaponsmith = new BehaviorToken("merchant_response_3");
+
+    // base game dialogue
+    public static BehaviorToken MerchantBarber = new BehaviorToken("barber_question1");
+
+    // base game dialogue
+    public static BehaviorToken MerchantShopworker = new BehaviorToken("shopworker_npc_player");
+
+    // base game dialogue
+    public static BehaviorToken MerchantBlacksmith = new BehaviorToken("blacksmith_player");
+    
+    // base game dialogue
+    public static BehaviorToken TavernKeeper = new BehaviorToken("tavernkeeper_talk");
+        
+    // base game dialogue
+    public static BehaviorToken TavernMaid = new BehaviorToken("tavernmaid_talk");
+
+    // base game dialogue
+    public static BehaviorToken TavernBard = new BehaviorToken("talk_bard_player");
+
+    // base game dialogue
+    public static BehaviorToken TavernGamehost = new BehaviorToken("taverngamehost_talk");
+
+    // base game dialogue
+    public static BehaviorToken TavernRansombroker = new BehaviorToken("ransom_broker_talk");
+
+    // base game dialogue
+    public static BehaviorToken TownOrVillageGeneral = new BehaviorToken("town_or_village_player");
+
+    // base game dialogue
+    public static BehaviorToken TownAlley = new BehaviorToken("alley_talk_start");
+
+    // base game dialogue
+    public static BehaviorToken TownArenamaster = new BehaviorToken("arena_master_talk");
+}
+
+public class BehaviorToken
+{
+    private readonly String _inputToken;
+
+    public BehaviorToken(String inputToken)
+    {
+        this._inputToken = inputToken;
+    }
+
+    private String InputToken()
+    {
+        return _inputToken;
+    }
+}
---
+++ BannerlordEnhancedFramework/BannerlordEnhancedFramework/src/extendedtypes/ExtendedTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace BannerlordEnhancedFramework.extendedtypes;
+
+public class ExtendedTimer : Timer
+{
+    private Action _timerJobDoCallback;
+    
+    public bool Active = false;
+    private bool _timerJobActive;
+
+    public ExtendedTimer(int interval, Action timerJobDoCallback) // pass in method with no arguments and no return value(void)
+    {
+        this._timerJobDoCallback = timerJobDoCallback;
+        this.Interval = interval;
+
+        // Hook up the Elapsed event for the timer.
+        this.Elapsed += BaseTimerJob;
+    }
+
+    private void BaseTimerJob(object source, ElapsedEventArgs e)
+    {
+        if (!_timerJobActive)
+        {
+            _timerJobActive = true;
+            this._timerJobDoCallback();
+            _timerJobActive = false;
+        }
+    }
+
+}
