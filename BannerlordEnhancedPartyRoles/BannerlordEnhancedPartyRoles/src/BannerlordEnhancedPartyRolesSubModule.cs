ae09a3db33d35299d7be368ac404167f1fcecb56
ricomaster9000 <ricorpwd@yahoo.co.za>
2023-06-20 16:20:42 +0200

    initial setup for EnhancedScout feature

--- BannerlordEnhancedPartyRoles/BannerlordEnhancedPartyRoles/src/BannerlordEnhancedPartyRolesSubModule.cs
+++ BannerlordEnhancedPartyRoles/BannerlordEnhancedPartyRoles/src/BannerlordEnhancedPartyRolesSubModule.cs
@@ -17,17 +17,8 @@ public class BannerlordEnhancedPartyRolesSubModule : MBSubModuleBase
     protected override void OnSubModuleLoad()
     {
         base.OnSubModuleLoad();
-            
         DebugUtils.LogAndPrintInfo("OnSubModuleLoad ->");
         HarmonyPatcher harmonyPatcher = new HarmonyPatcher("BannerlordEnhancedPartyRoles");
-
-        // TODO - remove or find a purpose or ignore
-        // ConfigurationUtils.LoadValues();
-        if (DebugUtils.IsDebugModeActive())
-        {
-            DebugUtils.PrintMethodNames(typeof(InventoryLogic));
-        }
-        DebugUtils.PrintMethodNames(typeof(InventoryLogic));
 
         DebugUtils.LogAndPrintInfo("add harmony patches - BEGIN");
         harmonyPatcher.PatchMethodPostfix(
@@ -35,31 +26,19 @@ public class BannerlordEnhancedPartyRolesSubModule : MBSubModuleBase
             typeof(QuartermasterPatches).GetMethod("DoneLogic_Postfix")
         );
         DebugUtils.LogAndPrintInfo("add harmony patches - END");
-
-        DebugUtils.LogAndPrintInfo("add dialogues - BEGIN");
-
-        // Kenneth...
-
-        DebugUtils.LogAndPrintInfo("add dialogues - END");
     }
 
     public override void OnGameLoaded(Game game, object initializerObject)
     {
         base.OnGameLoaded(game, initializerObject);
 
-        InformationManager.DisplayMessage(new InformationMessage("OnGameLoaded-------------------------------->!", Color.White));
+        DebugUtils.LogAndPrintInfo("OnGameLoaded-------------------------------->!");
     }
 
     protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
     {
         base.OnGameStart(game, gameStarterObject);
         if (!(game.GameType is Campaign)) return;
-
         if (gameStarterObject != null)  (gameStarterObject as CampaignGameStarter).AddBehavior(new QuaterMasterDialog());
-
-        // gameStarterObject.AddModel(new MoneyHandler());
-
-        MobileParty playerParty = Campaign.Current.MainParty;
-        Debug.Print(playerParty.ToString(), 0);
     }
 }
