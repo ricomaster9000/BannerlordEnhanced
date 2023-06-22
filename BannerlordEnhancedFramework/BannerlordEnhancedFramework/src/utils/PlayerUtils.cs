ded5874507731f2892bf05be9698e22cd62beb3e
ricomaster9000 <ricorpwd@yahoo.co.za>
2023-06-22 10:23:06 +0200

    added empty UiExtenderUtils, added PlayerUtils&WindowUtil classes

---
+++ BannerlordEnhancedFramework/BannerlordEnhancedFramework/src/utils/PlayerUtils.cs
@@ -0,0 +1,11 @@
+﻿using TaleWorlds.CampaignSystem.Party;
+
+namespace BannerlordEnhancedFramework.utils;
+
+public static class PlayerUtils
+{
+    public static bool IsPlayerHostileToParty(MobileParty mobileParty)
+    {
+        return MobileParty.MainParty.MapFaction.IsAtWarWith(mobileParty.MapFaction);
+    }
+}
