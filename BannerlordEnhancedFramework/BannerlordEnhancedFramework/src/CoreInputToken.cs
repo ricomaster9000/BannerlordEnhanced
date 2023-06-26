54b55fdf899964e0c5a32a9af9a9681283461edc
ricomaster9000 <ricorpwd@yahoo.co.za>
2023-06-26 22:43:44 +0200

    Codedacity code analysis - fix issues

--- BannerlordEnhancedFramework/BannerlordEnhancedFramework/src/CoreInputToken.cs
+++ BannerlordEnhancedFramework/BannerlordEnhancedFramework/src/CoreInputToken.cs
@@ -10,52 +10,52 @@ public class CoreInputToken
     {
         // base game dialogues
         // every hero should have this dialogue option, base game dialogue
-        public static InputToken HeroMainOptions = new InputToken("hero_main_options");
+        public static readonly InputToken HeroMainOptions = new InputToken("hero_main_options");
 
-        public static InputToken MerchantWeaponsmith = new InputToken("weaponsmith_talk_player");
+        public static readonly  InputToken MerchantWeaponsmith = new InputToken("weaponsmith_talk_player");
 
-        public static InputToken MerchantBarber = new InputToken("barber_question1");
+        public static readonly  InputToken MerchantBarber = new InputToken("barber_question1");
 
-        public static InputToken MerchantShopworker = new InputToken("shopworker_npc_player");
+        public static readonly  InputToken MerchantShopworker = new InputToken("shopworker_npc_player");
 
-        public static InputToken MerchantBlacksmith = new InputToken("blacksmith_player");
+        public static readonly  InputToken MerchantBlacksmith = new InputToken("blacksmith_player");
 
-        public static InputToken TavernKeeper = new InputToken("tavernkeeper_talk");
+        public static readonly  InputToken TavernKeeper = new InputToken("tavernkeeper_talk");
 
-        public static InputToken TavernMaid = new InputToken("tavernmaid_talk");
+        public static readonly  InputToken TavernMaid = new InputToken("tavernmaid_talk");
 
-        public static InputToken TavernBard = new InputToken("talk_bard_player");
+        public static readonly  InputToken TavernBard = new InputToken("talk_bard_player");
 
-        public static InputToken TavernGamehost = new InputToken("taverngamehost_talk");
+        public static readonly  InputToken TavernGamehost = new InputToken("taverngamehost_talk");
 
-        public static InputToken TavernRansombroker = new InputToken("ransom_broker_talk");
+        public static readonly  InputToken TavernRansombroker = new InputToken("ransom_broker_talk");
 
-        public static InputToken TownOrVillageGeneral = new InputToken("town_or_village_player");
+        public static readonly  InputToken TownOrVillageGeneral = new InputToken("town_or_village_player");
 
-        public static InputToken TownAlley = new InputToken("alley_talk_start");
+        public static readonly  InputToken TownAlley = new InputToken("alley_talk_start");
 
-        public static InputToken TownArenamaster = new InputToken("arena_master_talk");
+        public static readonly  InputToken TownArenamaster = new InputToken("arena_master_talk");
     }
 
     public class End
     {
         
         // generic, can apply anywhere, name pretty self explantory
-        public static InputToken CloseWindow = new InputToken("close_window");
+        public static readonly  InputToken CloseWindow = new InputToken("close_window");
 
-        public static InputToken Exit = new InputToken("end");
+        public static readonly  InputToken Exit = new InputToken("end");
         
-        public static InputToken HeroReturnToStart = new InputToken("lord_pretalk");
+        public static readonly  InputToken HeroReturnToStart = new InputToken("lord_pretalk");
 
-        public static InputToken MerchantBackToStart = new InputToken("merchant_response_3");
+        public static readonly  InputToken MerchantBackToStart = new InputToken("merchant_response_3");
 
-        public static InputToken MerchantShopworkerBackToStart = new InputToken("start_2");
+        public static readonly  InputToken MerchantShopworkerBackToStart = new InputToken("start_2");
 
-        public static InputToken MerchantBlacksmithBackToStart = new InputToken("player_blacksmith_after_craft");
+        public static readonly  InputToken MerchantBlacksmithBackToStart = new InputToken("player_blacksmith_after_craft");
 
-        public static InputToken TavernKeeperReturnToStart = new InputToken("tavernkeeper_pretalk");
+        public static readonly  InputToken TavernKeeperReturnToStart = new InputToken("tavernkeeper_pretalk");
 
-        public static InputToken TownOrVillageBackToStart = new InputToken("town_or_village_pretalk");
+        public static readonly  InputToken TownOrVillageBackToStart = new InputToken("town_or_village_pretalk");
     }
 
     public class InputToken
